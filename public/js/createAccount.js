80e9dd6b1d12a5c37ca181946d470308313f1096
David Acevedo <58893468+daceCS@users.noreply.github.com>
2023-04-03 14:04:08 -0700

    restricted charset

--- public/js/createAccount.js
+++ public/js/createAccount.js
@@ -1,5 +1,12 @@
 let clientAccountServerIndex;
-
+$('#username').on('keypress', function (event) {
+  var regex = new RegExp("^[a-zA-Z0-9]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+     event.preventDefault();
+     return false;
+  }
+});
 function createAccount(){
     let username = $("#username").val();
     let password = $("#password").val();
