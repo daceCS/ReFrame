d80b92f358174243ec4ca8493b44111901895ebb
louis-129 <128095517+louis-129@users.noreply.github.com>
2023-04-04 13:50:20 -0700

    added button function

--- public/js/user.js
+++ public/js/user.js
@@ -3,122 +3,138 @@
 
 
 $(document).ready(()=>{
-    let userPageObj;
-    let pathname = window.location.pathname;
-    let paths = pathname.split('/');
-    let userPage = paths[2];
-    let userPageUsername;
-    let userPageFollowCount; 
-    let userPagePostCount;
-    let userPageBio;
-    let allPost;
-    let userBannerImg;
-    let userProfileIcon;
-    $.get('/get-current-users', {username: userPage}, (data)=>{
-        userPageObj = data.account;
-        userPageUsername = userPageObj.username;
-        userPageFollowCount = userPageObj.following.length;
-        userPagePostCount = userPageObj.postCount;
-        userPageBio = userPageObj.bio;
-        allPost = userPageObj.posts;
-        userBannerImg = userPageObj.bannerImage;
-        userProfileIcon = userPageObj.profileIcon;
-        $('title').html(userPageUsername);
-        let userCard = `<div class="user-info">
-        <img class="banner" src="${userBannerImg}">
-        <img class="profileIcon" src="${userProfileIcon}">
-        <p id="username">${userPageUsername}</p>
-        <p id="follow-count">Followers: ${userPageFollowCount}</p>
-        <p id="post-count">Posts: ${userPagePostCount}</p>
+  let userPageObj;
+  let pathname = window.location.pathname;
+  let paths = pathname.split('/');
+  let userPage = paths[2];
+  let userPageUsername;
+  let userPageFollowCount; 
+  let userPagePostCount;
+  let userPageBio;
+  let allPost;
+  let userBannerImg;
+  let userProfileIcon;
+  $.get('/get-current-users', {username: userPage}, (data)=>{
+      userPageObj = data.account;
+      userPageUsername = userPageObj.username;
+      userPageFollowCount = userPageObj.followers;
+      userPagePostCount = userPageObj.postCount;
+      userPageBio = userPageObj.bio;
+      allPost = userPageObj.posts;
+      userBannerImg = userPageObj.bannerImage;
+      userProfileIcon = userPageObj.profileIcon;
+      $('title').html(userPageUsername);
+      let userCard = `<div class="user-info">
+      <img class="banner" src="${userBannerImg}">
+      <img class="profileIcon" src="${userProfileIcon}">
+      <p id="username">${userPageUsername}</p>
+      <p id="follow-count">Followers: ${userPageFollowCount}</p>
+      <p id="post-count">Posts: ${userPagePostCount}</p>
 
-        <p id="bio">${userPageBio}</p>
-    </div>`
-        let body = $(document.body);
-        body.append(userCard);
-        let feed = $('#feed');
-        for(i = 0; i<allPost.length; i++){
-      
-            if(allPost[i].inputType == 0){
-              let image = allPost[i].postData;
-              console.log(image)
-              let caption = allPost[i].caption;
-              let postedBy = allPost[i].postedBy;
-              let postIndex = allPost[i].postId;
-              postContainer = `<div id="post-${postIndex}" class="post-container">
-                                    <div class="title">
-                                      <h3 id="post-caption-${postIndex}" class="post-caption">${caption}</h3>
-                                    </div>
-                                    
-                                    <img src="${'../'+image}" id="post-image-${postIndex}" height="auto" width="90%"  class="post-image">
-                                    <div id="post-interact-${postIndex}" class="post-interact">
-                                      <input type="button" value="like">
-                                      <p id="user-id-${postIndex}" class="user-id">Post By: </p>  
-                                      <a href="/user/${postedBy.username}" class="user-link">${postedBy.username}</a>
-                                      
-                                    </div>
-                                    
-                                    
-                                  
-                                </div>`
-              
-             
-              feed.prepend(postContainer) 
-              
-
-             
+      <p id="bio">${userPageBio}</p>
+  </div>`
+      let body = $(document.body);
+      body.append(userCard);
+      let feed = $('#feed');
+      for(i = 0; i<allPost.length; i++){
     
-            }
-            else{
-              let text = allPost[i].postData;
-              let caption = allPost[i].caption;
-              let postedBy = allPost[i].postedBy;
-              let postIndex = allPost[i].postId;
-              postContainer = `<div id="post-${postIndex}" class="post-container">
-                                    <div class="title">
-                                      <h3 id="post-caption-${postIndex}" class="post-caption">${caption}</h3>
-                                    </div>
-                                    <p id="text-post">${text}</p>
-                                    <div id="post-interact-${postIndex}" class="post-interact">
-                                      <input type="button" value="like">
-                                      <p id="user-id-${postIndex}" class="user-id">Post By: </p>  
-                                      <a href="/user/${postedBy.username}" class="user-link">${postedBy.username}</a>
-                                      
-                                    </div>
-                                    
-                                    
+          if(allPost[i].inputType == 0){
+            let image = allPost[i].postData;
+            console.log(image)
+            let caption = allPost[i].caption;
+            let postedBy = allPost[i].postedBy;
+            let postIndex = allPost[i].postId;
+            postContainer = `<div id="post-${postIndex}" class="post-container">
+                                  <div class="title">
+                                    <h3 id="post-caption-${postIndex}" class="post-caption">${caption}</h3>
+                                  </div>
                                   
-                                </div>`
-                  
-              
-              feed.prepend(postContainer) 
-              
-              
-            }
+                                  <img src="${'../'+image}" id="post-image-${postIndex}" height="auto" width="90%"  class="post-image">
+                                  <div id="post-interact-${postIndex}" class="post-interact">
+                                    <input type="button" value="like">
+                                    <p id="user-id-${postIndex}" class="user-id">Post By: </p>  
+                                    <a href="/user/${postedBy.username}" class="user-link">${postedBy.username}</a>
+                                    
+                                  </div>
+                                  
+                                  
+                                
+                              </div>`
+            
+           
+            feed.prepend(postContainer) 
+            
 
+           
+  
+          }
+          else{
+            let text = allPost[i].postData;
+            let caption = allPost[i].caption;
+            let postedBy = allPost[i].postedBy;
+            let postIndex = allPost[i].postId;
+            postContainer = `<div id="post-${postIndex}" class="post-container">
+                                  <div class="title">
+                                    <h3 id="post-caption-${postIndex}" class="post-caption">${caption}</h3>
+                                  </div>
+                                  <p id="text-post">${text}</p>
+                                  <div id="post-interact-${postIndex}" class="post-interact">
+                                    <input type="button" value="like">
+                                    <p id="user-id-${postIndex}" class="user-id">Post By: </p>  
+                                    <a href="/user/${postedBy.username}" class="user-link">${postedBy.username}</a>
+                                    
+                                  </div>
+                                  
+                                  
+                                
+                              </div>`
+                
+            
+            feed.prepend(postContainer) 
             
             
-        }
+          }
 
-    });
+          
+          
+      }
 
-    if(localStorage.getItem('clientAccountIndex') == null){
-      let loginElement = `<a href="/login" id="profile-link">Profile</a>`;
+  });
+
+  
+
+
+  if(localStorage.getItem('clientAccountIndex') == null){
+    let loginElement = `<a href="/login" id="profile-link">Profile</a>`;
+    let menu = $('.menu');
+    menu.append(loginElement);
+  }
+  else if(localStorage.getItem('clientAccountIndex') != null){
+    let userIndex = localStorage.getItem('clientAccountIndex');
+    $.get('/get-user', {userIndex: userIndex}, (data)=>{
+      let loginElement = `<a href="/user/${data.userAccount.username}" id="profile-link">Profile</a>`;
       let menu = $('.menu');
-      menu.append(loginElement);
-    }
-    else if(localStorage.getItem('clientAccountIndex') != null){
-      let userIndex = localStorage.getItem('clientAccountIndex');
-      $.get('/get-user', {userIndex: userIndex}, (data)=>{
-        let loginElement = `<a href="/user/${data.userAccount.username}" id="profile-link">Profile</a>`;
-        let menu = $('.menu');
-        
-        menu.append(loginElement);
-      })
       
-    }
-   
-   
+      menu.append(loginElement);
+    })
     
+  }
+ 
+ 
+  
 
 
 });
+
+function changeFollowStatus(){
+  $('#Follow-User').click(function() { 
+    change(); 
+ });
+
+ function change() { 
+  if($('#Follow-User').val() == "Unfollow")
+    $('#Follow-User').val("Follow");
+  else if($('#Follow-User').val() == "Follow")
+    $('#Follow-User').val("Unfollow");
+ }
+}
